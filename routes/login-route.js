426467f0b34649d0720673a1aa3807676701ea86
Jamilah Alshammari <114332989+alshammariJa@users.noreply.github.com>
2022-12-03 21:03:22 +0300

    done

--- routes/login-route.js
+++ routes/login-route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const ActivityModel = require("../models/login.model");
+const userModel = require("../models/login.model");
 
-// Add an activity
+
 router.post("/add", function (req, res) {
   let newDocument = {
     email: req.body.title,
-    password: req.body.description,
+    password: req.body.password,
   
   };
 
@@ -27,14 +27,14 @@ router.put("/updateProfile", function (req, res) {
         _id: req.body._id,
       },
       {
-        email: req.body.title,
-        password: req.body.description,
+        email: req.body.email,
+        password: req.body.password,
       
       },
       { new: true }
     )
       .then(
-        // If success
+      
         function (dbDocument) {
           res.json(dbDocument);
         }
